34e18b1437b5447de66f4d1df41a46c95c6e006f
Jonatan Raudsepp <jonatan.raudsepp@gmail.com>
2013-10-09 17:23:19 +0300

    Tegin ära build.xml, tuleks mõelda millele ta versiooni numbrid lisab jne; aga seda arutab varsti.

---
+++ Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/Intro.java
@@ -0,0 +1,7 @@
+package ee.ut.math.tvt.LaheLabor;
+
+public class Intro {
+	public static void main(String [] args){
+		System.out.println("tere");
+	}
+}
---
+++ Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/IntroUI.java
@@ -0,0 +1,5 @@
+package ee.ut.math.tvt.LaheLabor;
+
+public class IntroUI {
+
+}
