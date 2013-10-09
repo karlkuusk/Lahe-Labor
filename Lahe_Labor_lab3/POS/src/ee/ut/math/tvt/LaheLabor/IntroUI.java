04419fc52eee4fa73c6d70ef953bf11cd9a0a48f
Rene Roost <roost.rene@gmail.com>
2013-10-09 19:29:54 +0300

    Arendasin

--- Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/IntroUI.java
+++ Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/IntroUI.java
@@ -3,32 +3,32 @@ package ee.ut.math.tvt.LaheLabor;
 import java.awt.Container;
 import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class IntroUI extends JFrame {
 	 IntroUI(){
-	 // raami loomine
 	 
-	    this.setSize(300, 300); // 
-	    this.setLocation(100, 100); // 
+	    this.setSize(300, 300); 
+	    this.setLocation(100, 100); 
 
-	    Container content = this.getContentPane(); // konteineri loomine
-	    content.setLayout(new FlowLayout()); // paigutushalduri kasutamine
+	    Container content = this.getContentPane(); 
+	    content.setLayout(new FlowLayout());
 
-	    JLabel label1 = new JLabel("Lahe Labor"); //
-	    JLabel label2 = new JLabel("Rene Roost"); //
-	    JLabel label3 = new JLabel("roost.rene@gmail.com"); //
-	    JLabel label4 = new JLabel("Lisbet, Jonatan, Karl, Rene"); //
-	    
-	    JLabel label5= new JLabel("1.0.0"); //
+	    JLabel label1 = new JLabel("Lahe Labor"); 
+	    JLabel label2 = new JLabel("Rene Roost"); 
+	    JLabel label3 = new JLabel("roost.rene@gmail.com"); 
+	    JLabel label4 = new JLabel("Team members: Liisbet Jürlau, Jonatan Raudsepp, Karl Kuusk, Rene Roost"); 
+	    JLabel label5= new JLabel("1.0.0"); 
 
-	          content.add(label1); // loodud elementide lisamine aknale
-	          content.add(label2);
-	          content.add(label3);
-	          content.add(label4);
-	          content.add(label5);
-	// 
+	    content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+	    content.add(label1);
+	    content.add(label2);
+	    content.add(label3);
+	    content.add(label4);
+	    content.add(label5);
 	          this.setVisible(true); 
 	 }
 }
