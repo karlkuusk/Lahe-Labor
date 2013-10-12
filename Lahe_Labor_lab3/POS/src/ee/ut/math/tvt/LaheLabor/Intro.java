b5e8479d9d3ba1f38f58393aa99289c0fad8e11d
karlkuusk <karlkuusk@hotmail.com>
2013-10-12 13:05:01 +0300

    Lisasin IntroUI-sse taustapildi(tiimilogo) Läheb käima ja Build töötab, aga kui läbi Ant´i käima panna siis tuleb classpathref error. Jon äkki saad jagu ?

--- Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/Intro.java
+++ Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/Intro.java
@@ -1,13 +1,8 @@
 
 package ee.ut.math.tvt.LaheLabor;
 
-
-import java.awt.*;
-import javax.swing.*;
-
 public class Intro {
 	  public static void main(String[] args) {
-		  new IntroUI();
-		    
+		  new IntroUI();		    
 		  }
 }
--- Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/IntroUI.java
+++ Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/IntroUI.java
@@ -1,26 +1,27 @@
 package ee.ut.math.tvt.LaheLabor;
 
-import java.awt.Container;
-import java.awt.FlowLayout;
 
+import java.awt.FlowLayout;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+
 
 public class IntroUI extends JFrame {
 	 IntroUI(){
 	 
 	    this.setSize(300, 300); 
-	    this.setLocation(100, 100); 
-
-	    Container content = this.getContentPane(); 
+	    this.setLocation(100, 100); 	    
+	    
+	    JLabel content=new JLabel(new ImageIcon(getClass().getResource("team.jpg")));
 	    content.setLayout(new FlowLayout());
+	    add(content);
 
 	    JLabel label1 = new JLabel("Lahe Labor"); 
 	    JLabel label2 = new JLabel("Rene Roost"); 
 	    JLabel label3 = new JLabel("roost.rene@gmail.com"); 
-	    JLabel label4 = new JLabel("Team members: Liisbet Jyyyyyyrlau, Jonatan Raudsepp, Karl Kuusk, Rene Roost"); 
+	    JLabel label4 = new JLabel("Team members: Liisbet Jyrlau, Jonatan Raudsepp, Karl Kuusk, Rene Roost"); 
 	    JLabel label5= new JLabel("1.0.0"); 
 
 	    content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
