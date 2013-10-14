7abf4e58117a8201d8e4941cb3106b7ace562e6e
Jonatan Raudsepp <jonatan.raudsepp@gmail.com>
2013-10-14 08:58:10 +0300

    Pm lõpetasin tegevuse build.xml ja kõige seonduvaga. Build.xml ning application.properties tuleks üle käia, võib kordusi olla või halba nime valikut vms. Ant targetid töötavad kõik mis vaja. Samuti tegin IntroUI siukseks, et ver number oleks õige, Liisu tehtud pilt jne. Tuleks see ilusaks ka teha, pm küllap ma teen, aga uus pilt pidavat tekkima, et äkki ootab selle ära. Jon

--- Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/IntroUI.java
+++ Lahe_Labor_lab3/POS/src/ee/ut/math/tvt/LaheLabor/IntroUI.java
@@ -2,27 +2,42 @@ package ee.ut.math.tvt.LaheLabor;
 
 
 import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
+import javax.swing.*;
+import com.jgoodies.looks.windows.WindowsLookAndFeel;
+import org.apache.log4j.Logger;
+import java.awt.*;
+import java.io.*;
 
 public class IntroUI extends JFrame {
-	 IntroUI(){
+	
+	private static final Logger log = Logger.getLogger(IntroUI.class.getName());
+	private String build_number;
+	private BufferedReader br;
+	
+	IntroUI(){
 	 
-	    this.setSize(300, 300); 
-	    this.setLocation(100, 100); 	    
+	    this.setSize(434, 500); 
+	    this.setLocation(100, 100);
+	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+	    JLabel content=new JLabel(new ImageIcon(getClass().getResource("/lahelabor.jpg")));
 	    
-	    JLabel content=new JLabel(new ImageIcon(getClass().getResource("team.jpg")));
 	    content.setLayout(new FlowLayout());
+	    
+	    GridBagConstraints c = new GridBagConstraints();
+
+	    
 	    add(content);
 
+		
+		readBuildNumber();
+		
 	    JLabel label1 = new JLabel("Lahe Labor"); 
 	    JLabel label2 = new JLabel("Rene Roost"); 
 	    JLabel label3 = new JLabel("roost.rene@gmail.com"); 
 	    JLabel label4 = new JLabel("Team members: Liisbet Jyrlau, Jonatan Raudsepp, Karl Kuusk, Rene Roost"); 
-	    JLabel label5= new JLabel("1.0.0"); 
+	    JLabel label5= new JLabel(build_number); 
 
 	    content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
 	    content.add(label1);
@@ -30,7 +45,33 @@ public class IntroUI extends JFrame {
 	    content.add(label3);
 	    content.add(label4);
 	    content.add(label5);
+	
+		try {
+	        UIManager.setLookAndFeel(new WindowsLookAndFeel());
+
+	    } catch (UnsupportedLookAndFeelException e1) {
+	        log.warn(e1.getMessage());
+	    }
+	    
 	    this.setVisible(true);
 	    
 	 }
+	
+
+	 private void readBuildNumber(){
+		try{
+			br=new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/version.properties")));
+			String tmp;
+			while((tmp=br.readLine())!=null){
+				if(tmp.startsWith("build.number=")){
+					build_number=tmp.split("build.number=")[1];
+					break;
+				}
+			}	
+			br.close();
+		}
+		catch(IOException e2){
+			log.warn(e2.getMessage());
+		}
+	}
 }
